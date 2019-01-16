7443cf8e987ca353e2d05337a4eb986b1a5dce19
林有勋 <linyouxun@yoju360.com>
2019-01-16 18:56:33 +0800

    [修改]shouye

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,35 +1,153 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
+const { stars, bed, time, house } = require('../../utils/const.js');
 
 Page({
   data: {
+    stars: stars,
+    bed: bed, 
+    time: time,
+    house: house,
+    windowHeight: 1206,
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    controls: [{
-      id: 1,
-      iconPath: '/resources/location.png',
-      position: {
-        left: 0,
-        top: 300 - 50,
-        width: 50,
-        height: 50
+    latitude:22.547951,
+    longitude:114.127348,
+    // 标注信息
+    markers: [{
+      callout: {
+        content: '选择中心点',
+        color: '#aaa',
+        padding: '5px',
+        borderWidth: '1px',
+        borderColor: '#efefef',
+        borderRadius: '5px',
       },
-      clickable: true
-    }]
+      id: 2,
+      latitude: 22.547951,
+      longitude: 114.127348,
+      name: '选择中心点',
+      iconPath: '/images/location.png'
+    // }, {
+    //   callout: {
+    //     content: '汉庭酒店(广州天河店)2\r\n 地址:广州市天河区天河路97-99号\r\n 电话:(020)22132600',
+    //     color: '#aaa',
+    //     padding: '5px',
+    //     borderWidth: '1px',
+    //     borderColor: '#efefef',
+    //     borderRadius: '5px',
+    //   },
+    //   // label: {
+    //   //   content: '汉庭酒店1',
+    //   //   fontSize: '25rpx',
+    //   //   color: '#ff0000'
+    //   // },
+    //   id: 1,
+    //   latitude: 23.10346770738263,
+    //   longitude: 113.33679378845213,
+    //   name: '汉庭酒店(广州天河店)',
+    //   iconPath: '/images/hotel.png'
+    }],
+    // circles: {
+    //   latitude: 23.10346770738263,
+    //   longitude: 113.33679378845213,
+    //   color: '#0f0',
+    //   fillColor: '#f0f',
+    //   radius: 10,
+    //   strokeWidth: 1
+    // }
+    more: false,
+    // 统计信息
+    money: 0,
+    startDate: '',
+    endDate: '',
+    position: '',
+    starIndex: 0,
+    bedIndex: 0,
+    timeIndex: 0,
+    houseIndex: 0,
+  },
+  resetData() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = 11;
+    console.log(day);
+    const dateStr = year + '-' + '00'.substr(0, 2 - ((month + 1) + '').length) + (month + 1) + '-' + '00'.substr(0, 2- (day + '').length) + day ;
+    this.setData({
+      money: 0,
+      startDate: dateStr,
+      endDate: dateStr,
+      starIndex: 0,
+      bedIndex: 0,
+      timeIndex: 0,
+      houseIndex: 0,
+    })
+  },
+  sendForm() {
+    const { money, startDate, endDate, starIndex, bedIndex, timeIndex, houseIndex } = this.data;
+    console.log(money, startDate, endDate, starIndex, bedIndex, timeIndex, houseIndex);
+  },
+  bindChange(e) {
+    this.setData({
+      [e.target.dataset.name]: e.detail.value
+    })
+  },
+  bindtap(e) {
+  },
+  moreInfo() {
+    const { more } = this.data;
+    this.setData({
+      more: !more
+    })
+  },
+  translateMarker: function () {
+    this.mapCtx.translateMarker({
+      markerId: 1,
+      autoRotate: true,
+      duration: 1000,
+      destination: {
+        latitude: 23.10229,
+        longitude: 113.3345211,
+      },
+      animationEnd() {
+        console.log('animation end')
+      }
+    })
+  },
+  markertap(e) {
+    console.log(e)
   },
   controltap(e) {
     console.log(e.controlId)
   },
   regionchange(e) {
-    console.log(e.type)
+    this.mapCtx.getCenterLocation({
+      success:(res) => {
+        console.log(res.longitude)
+        console.log(res.latitude)
+        const markers = this.data.markers;
+        markers[0].longitude = res.longitude;
+        markers[0].latitude = res.latitude;
+        this.setData({
+          markers
+        })
+      }
+    })
   },
   //事件处理函数
   bindViewTap: function() {
     console.log('更新头像信息');
   },
   onLoad: function () {
+    this.setData({
+      windowHeight: 750 / wx.getSystemInfoSync().windowWidth * wx.getSystemInfoSync().windowHeight
+    });
+    this.resetData();
+    this.mapCtx = wx.createMapContext('map');
+    // this.mapCtx.moveToLocation();
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
---
+++ utils/const.js
@@ -0,0 +1,10 @@
+const stars = [{ id: '1', name: '全部' }, { id: '2', name: '三星以下' }, { id: '3', name: '三星' }, { id: '4', name: '四星' }, { id: '5', name: '五星' }, { id: '6', name: '六星' }];
+const bed = [{ id: '1', name: '单床房' }, { id: '2', name: '双床房' }, { id: '3', name: '套房' }];
+const time = [{ id: '1', name: '17:00' }, { id: '2', name: '18:00' }, { id: '3', name: '19:00' }, { id: '4', name: '20:00' }, { id: '5', name: '21:00' }, { id: '6', name: '22:00' }, { id: '7', name: '23:00' }, { id: '8', name: '次日00:00' }, { id: '9', name: '次日01:00' }, { id: '10', name: '次日02:00' }, { id: '11', name: '次日03:00' }, { id: '12', name: '次日04:00' }, { id: '13', name: '次日05:00' }, { id: '14', name: '次日06:00' }];
+const house = [{ id: '1', name: '1间' }, { id: '2', name: '2间' }, { id: '3', name: '3间' }, { id: '4', name: '4间' }, { id: '5', name: '5间' }, { id: '6', name: '6间' }, { id: '7', name: '7间' }, { id: '8', name: '8间' }, { id: '9', name: '9间' }, { id: '10', name: '10间' }]
+module.exports = {
+  stars: stars,
+  bed: bed,
+  time: time,
+  house: house,
+}
